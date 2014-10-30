7d4b358b204132e677ab43a536ab648a373a77ae
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-10-30 23:18:03 +1300

    Fixing errors in the remote verification script.

--- scripts/test.integration.master/Test-AzureJenkinsMaster.ps1
+++ scripts/test.integration.master/Test-AzureJenkinsMaster.ps1
@@ -7,8 +7,8 @@ function New-Error{
     )
 
     $errorResult = New-Object PSObject
-    $errorResult = Add-Member -InputObject $errorResult -MemberType NoteProperty -Name Message -Value $errorText
-    $errorResult = Add-Member -InputObject $errorResult -MemberType ScriptMethod -Name Write -Value { Write-Error $this.Message }
+    Add-Member -InputObject $errorResult -MemberType NoteProperty -Name Message -Value $errorText
+    Add-Member -InputObject $errorResult -MemberType ScriptMethod -Name Write -Value { Write-Error $this.Message }
     
     $errorResult
 }
@@ -19,43 +19,54 @@ function New-Information{
     )
 
     $infoResult = New-Object PSObject
-    $infoResult = Add-Member -InputObject $infoResult -MemberType NoteProperty -Name Message -Value $infoText
-    $infoResult = Add-Member -InputObject $infoResult -MemberType ScriptMethod -Name Write -Value { Write-Output $this.Message }
+    Add-Member -InputObject $infoResult -MemberType NoteProperty -Name Message -Value $infoText
+    Add-Member -InputObject $infoResult -MemberType ScriptMethod -Name Write -Value { Write-Output $this.Message }
     
     $infoResult
 }
 
-$result = New-Object PSObject
-$result = Add-Member -InputObject $result -MemberType NoteProperty -Name HasPassed -Value $true
-$result = Add-Member -InputObject $result -MemberType NoteProperty -Name Log -Value @( )
-
-$dir = "c:\temp"
-if (-not (Test-Path $dir))
+try
 {
-    $result.HasPassed = $false
-    $result.Log += New-Error "The temp directory was not created"
+    $result = New-Object PSObject
+    Add-Member -InputObject $result -MemberType NoteProperty -Name HasPassed -Value $true
+    Add-Member -InputObject $result -MemberType NoteProperty -Name Log -Value @( )
+
+    $dir = "c:\temp"
+    if (-not (Test-Path $dir))
+    {
+        $result.HasPassed = $false
+        $result.Log += New-Error "The temp directory was not created"
+        return $result
+    }
+
+    $result.Log += New-Information "The temp directory was created successfully"
+
+    $file = Join-Path $dir "myfile.txt"
+    if (-not (Test-Path $file))
+    {
+        $result.HasPassed = $false
+        $result.Log += New-Error "The test file was not created"
+        return $result
+    }
+
+    $result.Log += New-Information "The test file was created successfully"
+
+    $content = Get-Content -Path $file
+    if ($content -ne "This is a test")
+    {
+        $result.HasPassed = $false
+        $result.Log += New-Error "The test file contained the wrong content. Expected 'This is a test' but got: $content"
+        return $result
+    }
+
+    $result.Log += New-Information "The test file contained the correct content"
+    $result
+}
+catch
+{
+    $result = New-Object PSObject
+    $result = Add-Member -InputObject $result -MemberType NoteProperty -Name HasPassed -Value $true
+    $result = Add-Member -InputObject $result -MemberType NoteProperty -Name Log -Value @( New-Error "Script failed with error: $_" )
+
     return $result
 }
-
-$result.Log += New-Information "The temp directory was created successfully"
-
-$file = Join-Path $dir "myfile.txt"
-if (-not (Test-Path $file))
-{
-    $result.HasPassed = $false
-    $result.Log += New-Error "The test file was not created"
-    return $result
-}
-
-$result.Log += New-Information "The test file was created successfully"
-
-$content = Get-Content -Path $file
-if ($content -ne "This is a test")
-{
-    $result.HasPassed = $false
-    $result.Log += New-Error "The test file contained the wrong content. Expected 'This is a test' but got: $content"
-    return $result
-}
-
-$result.Log += New-Information "The test file contained the correct content"
-$result
