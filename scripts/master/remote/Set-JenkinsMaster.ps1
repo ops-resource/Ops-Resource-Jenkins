dd9d1246a9a88a6b7711a0abe068e44a48e5b222
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-11-05 22:31:44 +1300

    Adding inline documentation to the Set-JenkinsMaster.ps1 script.

--- scripts/master/remote/Set-JenkinsMaster.ps1
+++ scripts/master/remote/Set-JenkinsMaster.ps1
@@ -1,3 +1,18 @@
+<#
+    .SYNOPSIS
+ 
+    Takes all the actions necessary to prepare a Windows machine for use as a Jenkins master.
+ 
+ 
+    .DESCRIPTION
+ 
+    The Set-JenkinsMaster script takes all the actions necessary to prepare a Windows machine for use as a Jenkins master
+ 
+ 
+    .EXAMPLE
+ 
+    Set-JenkinsMaster
+#>
 [CmdletBinding()]
 param()
 
