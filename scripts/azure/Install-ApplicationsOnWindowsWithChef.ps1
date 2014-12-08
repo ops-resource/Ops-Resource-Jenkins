29da1b47419ac2dcd6d0a35534b6ef00f6db59db
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-12-08 22:02:37 +1300

    Switching the remote install process over to install Ruby and the Chef gems and use it from there. This with the idea that we can then later on use ServerSpec for the testing.
    references #2

--- scripts/azure/Install-ApplicationsOnWindowsWithChef.ps1
+++ scripts/azure/Install-ApplicationsOnWindowsWithChef.ps1
@@ -20,38 +20,6 @@ param(
     [string] $cookbookName          = "jenkinsmaster"
 )
 
-function Install-Msi
-{
-    param(
-        [string] $msiFile,
-        [string] $logFile
-    )
-
-    $p = Start-Process -FilePath "msiExec.exe" -ArgumentList "/i $msiFile /Lime! $logFile /qn" -PassThru
-    $p.WaitForExit()
-
-    if ($p.ExitCode -ne 0)
-    {
-        throw "Failed to install: $msiFile"
-    }
-}
-
-function Uninstall-Msi
-{
-    param(
-        [string] $msiFile,
-        [string] $logFile
-    )
-
-    $p = Start-Process -FilePath "msiExec.exe" -ArgumentList "/x $msiFile /Lime! $logFile /qn" -PassThru
-    $p.WaitForExit()
-
-    if ($p.ExitCode -ne 0)
-    {
-        throw "Failed to install: $msiFile"
-    }
-}
-
 # The directory that contains all the installation files
 $installationDirectory = $PSScriptRoot
 
@@ -67,97 +35,65 @@ if (-not (Test-Path $logDirectory))
 
 
 # Install Chocolatey
+Write-Output "Installing chocolatey ..."
+Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Add chocolatey to the path
+$env:ChocolateyInstall = "C:\ProgramData\chocolatey"
+
 # Install ruby
+Write-Output "Installing ruby via chocolatey ..."
+& choco install ruby -version 2.1.3.0
+
+# Patch PATH for ruby
+$env:PATH += ";C:\tools\ruby213\bin"
+
 # install ruby2.devkit
+Write-Output "Installing ruby2.devkit via chocolatey ..."
+& choco install ruby2.devkit -version 4.7.2.2013022402
+
 # patch devkit config
+Write-Output "Patching ruby devkit config ..."
+Add-Content -Path "C:\tools\DevKit2\config.yml"  -Value " - C:\tools\ruby213"
+
 # rerun devkit install stuff
+Write-Output "Updating ruby with DevKit ..."
+& ruby "C:\tools\DevKit2\dk.rb" install
 
 # load gems
-# -chef
-# -chef-zero
-# -win32-process
+Write-Output "Installing win32-process gem ..."
+& gem install win32-process --version 0.7.4
 
-# Run chef-zero and push the cookbook to it
+Write-Output "Installing win32-service gem ..."
+& gem install win32-service --version 0.8.6
+
+Write-Output "Installing chef gem ..."
+& gem install chef --version 12.0.0
 
 
-# Download chef client. Note that this is obviously hard-coded but for now it will work. Later on we'll make this a configuration option
-Write-Output "Downloading chef installer ..."
-$chefClientInstallFile = "chef-windows-11.16.4-1.windows.msi"
-$chefClientDownloadUrl = "https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/x86_64/" + $chefClientInstallFile
-$chefClientInstall = Join-Path $installationDirectory $chefClientInstallFile
-Invoke-WebRequest -Uri $chefClientDownloadUrl -OutFile $chefClientInstall -Verbose
-
-Write-Output "Chef download complete."
-if (-not (Test-Path $chefClientInstall))
+# Set the path for the cookbooks
+$chefConfigDir = Join-Path $env:UserProfile ".chef"
+if (-not (Test-Path $chefConfigDir))
 {
-    throw 'Failed to download the chef installer.'
+    Write-Output "Creating the chef configuration directory ..."
+    New-Item -Path $chefConfigDir -ItemType Directory | Out-Null
 }
 
-# Install the chef client
-Unblock-File -Path $chefClientInstall
-
-Write-Output "Installing chef from $chefClientInstall ..."
-$chefInstallLogFile = Join-Path $logDirectory "chef.install.log"
-Install-Msi -msiFile "$chefClientInstall" -logFile "$chefInstallLogFile"
-
-try 
+$chefConfig = Join-Path $chefConfigDir 'knife.rb'
+if (-not (Test-Path $chefConfig))
 {
-    # Set the path for the cookbooks
-    $chefConfigDir = Join-Path $env:UserProfile ".chef"
-    if (-not (Test-Path $chefConfigDir))
-    {
-        Write-Output "Creating the chef configuration directory ..."
-        New-Item -Path $chefConfigDir -ItemType Directory | Out-Null
-    }
+    Write-Output "Creating the chef configuration file"
+    Set-Content -Path $chefConfig -Value ('cookbook_path ["' + $installationDirectory + '/cookbooks"]')
 
-    $chefConfig = Join-Path $chefConfigDir 'knife.rb'
-    if (-not (Test-Path $chefConfig))
-    {
-        Write-Output "Creating the chef configuration file"
-        Set-Content -Path $chefConfig -Value ('cookbook_path ["' + $installationDirectory + '/cookbooks"]')
-
-        # Make a copy of the config for debugging purposes
-        Copy-Item $chefConfig $logDirectory
-    }
-
-    $opscodePath = "c:\opscode"
-    if (-not (Test-Path $opscodePath))
-    {
-        throw "Chef install path not found."
-    }
-
-    # Add the ruby path to the $env:PATH for the current session.
-    $embeddedRubyPath = "$opscodePath\chef\embedded\bin"
-    if (-not (Test-Path $embeddedRubyPath))
-    {
-        throw "Embedded ruby path not found."
-    }
-
-    $env:PATH += ";" + $embeddedRubyPath
-
-    # Execute the chef client as: chef-client -z -o $cookbookname
-    $chefClient = "$opscodePath\chef\bin\chef-client.bat"
-    if (-not (Test-Path $chefClient))
-    {
-        throw "Chef client not found"
-    }
-
-    Write-Output "Running chef-client ..."
-    & $chefClient -z -o $cookbookName
-    if (($LastExitCode -ne $null) -and ($LastExitCode -ne 0))
-    {
-        throw "Chef-client failed. Exit code: $LastExitCode"
-    }
-
-    Write-Output "Chef-client completed."
+    # Make a copy of the config for debugging purposes
+    Copy-Item $chefConfig $logDirectory
 }
-finally
+
+Write-Output "Running chef-client ..."
+& chef-client -z -o $cookbookName
+if (($LastExitCode -ne $null) -and ($LastExitCode -ne 0))
 {
-    Write-Output "Uninstalling chef ..."
-
-    # delete chef from the machine
-    $chefUninstallLogFile = Join-Path $logDirectory "chef.uninstall.log"
-    Uninstall-Msi -msiFile $chefClientInstall -logFile $chefUninstallLogFile
+    throw "Chef-client failed. Exit code: $LastExitCode"
 }
+
+Write-Output "Chef-client completed."
