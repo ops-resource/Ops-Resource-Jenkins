c1570bb4e6a642968234a08fb7e70132dd4f79f7
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-12-22 12:13:53 +1300

    Automatically setting the version number and company name in the cookbook metadata.
    references #2

--- cookbooks/jenkinsmaster/metadata.rb
+++ cookbooks/jenkinsmaster/metadata.rb
@@ -1,7 +1,7 @@
 name             'jenkinsmaster'
-maintainer       'YOUR_COMPANY_NAME'
+maintainer       '${CompanyName} (${CompanyUrl})'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures jenkinsmaster'
+description      'Installs/Configures a Jenkins master on a Windows server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '${VersionSemantic}'
