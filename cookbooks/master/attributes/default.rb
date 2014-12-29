f5de94adf04684d27f454616bd986ce34b7a0992
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-12-30 00:16:22 +1300

    Grabbing the latest x64 version of 7-zip
    references #2

--- cookbooks/master/attributes/default.rb
+++ cookbooks/master/attributes/default.rb
@@ -2,8 +2,8 @@
 default['windows']['allow_pending_reboots'] = 'false'
 
 # Install 7-zip
-default['7-zip']['url'] = 'http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.35/7z935.msi'
-default['7-zip']['package_name'] = '7-Zip 9.35 (x64 edition)'
+default['7-zip']['url'] = 'http://downloads.sourceforge.net/project/sevenzip/7-Zip/9.36/7z936-x64.msi'
+default['7-zip']['package_name'] = '7-Zip 9.36 (x64 edition)'
 
 # install GIT
 default['git']['version'] = '1.9.5-preview20141217'
