3a9fc013dc677ba3f6b898492451859e634620e6
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-01-12 23:48:18 +1300

    Adding a try .. catch to the jenkins serverspec test to make sure the test completes even if there are issues with the jenkins service
    references #2

--- spec/master/jenkins_spec.rb
+++ spec/master/jenkins_spec.rb
@@ -58,10 +58,17 @@ end
 
 # Query the version of jenkins that is running
 describe 'jenkins webservice' do
-  response = RestClient.get 'http://localhost:8080/api/json'
-  it 'is active an returns the correct version' do
-    expect(response.headers.empty?).to be(false)
-    expect(response.headers.keys).to include(:x_jenkins)
-    expect(response.headers[:x_jenkins]).to eq('1.595')
+  begin
+    response = RestClient.get 'http://localhost:8080/api/json'
+    it 'is active an returns the correct version' do
+      expect(response.headers.empty?).to be(false)
+      expect(response.headers.keys).to include(:x_jenkins)
+      expect(response.headers[:x_jenkins]).to eq('1.595')
+    end
+  rescue
+    it 'fails' do
+      # this always fails because there was an exception of some kind
+      expect(false).to be true
+    end
   end
 end
